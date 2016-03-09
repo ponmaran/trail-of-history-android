82d07597246618bf5a51c0d5114cd64bf093f051
Trey Robinson <trey.robinson@skookum.com>
2016-03-09 09:06:34 -0500

    Added RecyclerView from March meetup.

---
+++ app/src/main/java/org/charmeck/trailofhistory/model/PointOfInterest.java
@@ -0,0 +1,15 @@
+package org.charmeck.trailofhistory.model;
+
+/**
+ * Created by Trey Robinson on 3/8/16.
+ */
+public class PointOfInterest {
+
+    public String name;
+    public String description;
+
+    public PointOfInterest(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/org/charmeck/trailofhistory/poi/PointOfInterestAdapter.java
@@ -0,0 +1,57 @@
+package org.charmeck.trailofhistory.poi;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.charmeck.trailofhistory.R;
+import org.charmeck.trailofhistory.model.PointOfInterest;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Trey Robinson on 3/8/16.
+ */
+public class PointOfInterestAdapter  extends RecyclerView.Adapter<PointOfInterestAdapter.ViewHolder>{
+
+    private List<PointOfInterest> pointOfInterestList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.poiName) TextView name;
+        @Bind(R.id.poiDescription) TextView description;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public PointOfInterestAdapter(List<PointOfInterest> pointOfInterestList) {
+        this.pointOfInterestList = pointOfInterestList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_point_of_interest, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.name.setText(pointOfInterestList.get(position).name);
+        holder.description.setText(pointOfInterestList.get(position).description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pointOfInterestList.size();
+    }
+}
