db4b4e4fa5330787c6f4f58a3c6b5c9efebe4fc5
Alexander Nohe <alex.nohe427@gmail.com>
2016-07-26 21:27:26 -0400

    Added a setting to check for whether or not the checkbox is checked and storing to shared preferences.

--- ranger/src/main/java/org/charmeck/trailofhistory/ranger/EmailPasswordActivity.java
+++ ranger/src/main/java/org/charmeck/trailofhistory/ranger/EmailPasswordActivity.java
@@ -7,12 +7,15 @@ package org.charmeck.trailofhistory.ranger;
  */
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.IntentCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -24,16 +27,26 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 public class EmailPasswordActivity extends BaseActivity {
 
     private static final String TAG = "EmailPassword";
 
+    //Preferences
+    private static final String CLTPREFERENCES = "CLTPREFERENCES";
+    private static final String REMEMBER_ID = "remember_id";
+    private static final String STORED_USERNAME = "stored_username";
+    //End Preferences
+
     @BindView(R.id.field_email) EditText mEmailField;
     @BindView(R.id.field_password) EditText mPasswordField;
+    @BindView(R.id.bool_remember_id) CheckBox mRememberId;
 
     private FirebaseAuth mAuth;
+    private SharedPreferences mSharedPreferences;
+    private boolean mRememberIdValue;
 
     public static Intent newInstance(Context context){
         Intent intent =  new Intent(context, EmailPasswordActivity.class);
@@ -47,8 +60,17 @@ public class EmailPasswordActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSharedPreferences = getSharedPreferences(CLTPREFERENCES, MODE_PRIVATE);
+        mRememberIdValue = mSharedPreferences.getBoolean(REMEMBER_ID, false);
         setContentView(R.layout.activity_emailpassword);
         ButterKnife.bind(this);
+
+        if (mRememberIdValue) {
+            Log.d(TAG, "testing setting text and checkbox");
+            mRememberId.setChecked(true);
+            mEmailField.setText(mSharedPreferences.getString(STORED_USERNAME, ""));
+            mPasswordField.requestFocus();
+        }
 
         mAuth = FirebaseAuth.getInstance();
     }
@@ -58,6 +80,8 @@ public class EmailPasswordActivity extends BaseActivity {
         if (!validateForm()) {
             return;
         }
+
+        storeCredentials(mSharedPreferences, email, mRememberId.isChecked());
 
         showProgressDialog();
 
@@ -105,6 +129,31 @@ public class EmailPasswordActivity extends BaseActivity {
         }
 
         return valid;
+    }
+
+    /**
+     * This is used to store the credentials for the user.  This only stores the username and only
+     * if the rememeber Id option is valid.  Otherwise, it removes it from the shared preferences so
+     * in the event someone does turn on the switch, they do not get a previous stored value.
+     *
+     * @param sharedPreferences - This is the shared preferences for the application.  Intentionally
+     *                          passing in as a parameter for di in the future.
+     * @param email - The email of the user to be stored.
+     * @param rememberId - The boolean value of the checkbox so it can be stored for future use and
+     *                   to retrieve the email in the onCreate method.
+     */
+    private void storeCredentials(SharedPreferences sharedPreferences, String email, boolean rememberId) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if(rememberId) {
+            Log.d(TAG, "storing username");
+            editor.putString(STORED_USERNAME, email).apply();
+            editor.putBoolean(REMEMBER_ID, true).apply();
+        }
+        else {
+            Log.d(TAG, "removing username");
+            editor.remove(STORED_USERNAME).apply();
+            editor.remove(REMEMBER_ID).apply();
+        }
     }
 
     @OnClick(R.id.email_sign_in_button)
