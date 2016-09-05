085a4c1295010fa99d861e331e1deca64eb96f8f
Antoine Campbell <acampbell@captechventures.com>
2016-09-05 18:38:09 -0400

    - Moved emulator management to build script from travis.yml

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 # Exit on error
 set -e
 
+# Emulator Management: Create, Start and Wait
+echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+emulator -avd test -no-audio -no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &
+
 # Copy mock google-services file if necessary
 if [ ! -f ./ranger/google-services.json ]; then
   echo "Using mock google-services.json"
