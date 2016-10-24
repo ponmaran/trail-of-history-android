f7b4b27b07a145d9ce9375b5788a52e1189ce5ed
Chris Ollenburg <chris.ollenburg@gmail.com>
2016-10-24 09:48:34 -0400

    Added basic map activity with view pager and markers for tof points of interest. Also updated the readme with new steps to enable the app to run.

---
+++ app/src/main/java/org/charmeck/trailofhistory/PoiPagerAdapter.java
@@ -0,0 +1,30 @@
+package org.charmeck.trailofhistory;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import java.util.List;
+import org.charmeck.trailofhistory.core.model.PointOfInterest;
+
+/**
+ * FragmentStatePagerAdapter for the map view cards.
+ *
+ * Created by Chris on 10/24/16.
+ */
+public class PoiPagerAdapter extends FragmentStatePagerAdapter {
+  private List<PointOfInterest> pois;
+
+  public PoiPagerAdapter(FragmentManager fm, List<PointOfInterest> pois) {
+    super(fm);
+    this.pois = pois;
+  }
+
+  @Override public Fragment getItem(int position) {
+    PointOfInterest poi = pois.get(position);
+    return PoiFragment.create(poi);
+  }
+
+  @Override public int getCount() {
+    return pois.size();
+  }
+}
