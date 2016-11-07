b14e036d2c21698856d35ab2e809f0efcef848bf
ponmaran <ponmaran@gmail.com>
2016-11-07 17:02:50 -0500

    Adding Toolbar to Main Activity

--- app/src/main/java/org/charmeck/trailofhistory/MainActivity.java
+++ app/src/main/java/org/charmeck/trailofhistory/MainActivity.java
@@ -1,11 +1,15 @@
 package org.charmeck.trailofhistory;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.google.firebase.database.DataSnapshot;
@@ -19,16 +23,19 @@ import org.charmeck.trailofhistory.core.ui.poi.list.PointOfInterestAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+  @BindView(R.id.toolbar) Toolbar toolbar;
   @BindView(R.id.recyclerView) RecyclerView recyclerView;
   PointOfInterestAdapter poiAdapter;
   private DatabaseReference databaseReference;
-
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.bind(this);
+
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     databaseReference = FirebaseDatabase.getInstance().getReference();
 
@@ -40,6 +47,21 @@ public class MainActivity extends AppCompatActivity {
 
   }
 
+  @Override public boolean onCreateOptionsMenu(Menu menu){
+    getMenuInflater().inflate(R.menu.view_option,menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()){
+      case R.id.option_view:
+        Intent listViewIntent = new Intent(getApplicationContext(),MapActivity.class);
+        startActivity(listViewIntent);
+        return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
 
 
   public void fetchStatues() {
